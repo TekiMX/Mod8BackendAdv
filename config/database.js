4f34607f12f19d38dab15c2af813119ea0c6e81b
Salvador Rodriguez <ale2ph@hotmail.com>
2024-05-01 16:23:44 -0600

    feature: add mongodb connection

---
+++ config/database.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+// read enviromentt variables
+dotenv.config()
+
+const connect = () => {
+  mongoose.connect(process.env.DB_CONNECT_URI) // conect to the database
+
+  const { connection } = mongoose // bring connection to check if it works
+
+  connection.once('open', () => {
+    console.log('✅ Database connection stablished')
+  })
+
+  connection.on('error', (error) => {
+    console.log('❌ database connecton error ❌')
+  })
+}
+
+export { connect }
