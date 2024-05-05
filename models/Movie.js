f8c15844c477d01962dd259133b723adbbb3d9a3
TekiMX <ale2ph@hotmail.com>
2024-05-05 17:02:07 -0600

    minor fixes movie model

--- models/Movie.js
+++ models/Movie.js
@@ -1,15 +1,16 @@
 // #1 importar mongoose
 import mongoose from 'mongoose'
 
-const genreEnum = ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Musicals', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'Western']
+const genreEnum = ['Action', 'Adventure', 'Comedy', 'Drama', 'Documental', 'Fantasy', 'Horror', 'Musicals', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'Western']
 // #2 definir un esquema
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
   director: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Director', required: true }], // ObjectID es un tipo de  dato utilizado por Mongoose para identificar documentos en MongoDb. ref: 'Author' indica que el campo authors se relaciona con el modelo Author
-  publishDate: { type: Date }, // YYYY-MM-DD
+  publishDate: { type: Date }, // YYYY-MM-DD format please
   length: { type: Number, required: true },
   score: { type: Number, required: true },
   genre: { type: String, required: true, enum: genreEnum },
+  storyline: { type: String, required: true },
   isActive: { type: Boolean, default: true }
 }, { timestamps: true }) // timestamps: true agrega createdAt y updatedAt
 
