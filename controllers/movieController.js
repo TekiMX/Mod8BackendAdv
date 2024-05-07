930bc806054294d1453b870fc6d3507916c80afb
Salvador Rodriguez <ale2ph@hotmail.com>
2024-05-06 23:09:46 -0600

    Working: creating movieController part1- Directors

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -18,8 +18,22 @@ const createMovie = async (req, res) => {
       return res.status(400).json({ msg: 'Director data should be an array' })
     }
 
-// Create Director if doesn't exist 
-    const 
+    // Create Directors one by one and save them in DB
+
+    const directorModels = await Promise.all(movieData.director.map(async director => {
+      // search if author exists
+      const existingDirector = await Director.findOne({ firstName: director.firstName, lastName: director.lastname, birthDate: director.birdDate })
+      // if author exists, retrieve it
+      if (existingDirector) {
+        return existingDirector
+      }
+      // if Director doens't exist, create it
+      const newDirector = new Director(director)
+      return await Director.create(newDirector)
+    }
+    ))
+
+    // Now than we save the Directors, we need to retrieve their ID to assing them to te movie
   } catch (err) {
     res.status(400).json({ error: err.message })
   }
